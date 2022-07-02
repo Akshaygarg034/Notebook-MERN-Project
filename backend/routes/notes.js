c49f129a72c104561a7e4589653a9e0d6a18c064
Akshay <20bcs59@nith.ac.in>
2022-07-02 16:35:08 +0530

    a

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -39,4 +39,27 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+router.put('/updatenote/:id',fetchuser,async (req, res) => {
+    const { title, description, tag } = req.body;
+    const newNote = {};
+    try {
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send('Not Found')
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('Not Allowed')
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json(note)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal server error');
+    }
+})
+
 module.exports = router
