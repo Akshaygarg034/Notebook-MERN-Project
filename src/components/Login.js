a713778cf0f34c4ff4126ada1f537aa4e8148902
Akshay <20bcs59@nith.ac.in>
2023-01-05 14:36:02 +0530

    sadsa

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ function Login(props) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/auth/login', {
+        const response = await fetch('http://localhost:5000/api/noteauth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
